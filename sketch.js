4146f35a4d989854a8993386ad734dbc507db3c8
Rushil <rushil.prasad10980@gmail.com>
2020-11-27 19:54:19 +0530

    added

--- sketch.js
+++ sketch.js
@@ -1,44 +1,63 @@
 //Create variables here
-var dog, happyDog
+var dog, happyDog, sadDog;
 var database
 var foodS, foodStock
+var fedTime, lastFed, foodObj;
 function preload()
 {
-  loadImage("doglmg.png");
-  loadImage("doglmg1.png");
+  sadDog=loadImage("images/dogImg.png");
+  happyDog=loadImage("images/dogImg1.png");
 }
 
 function setup() {
-	createCanvas(500, 500);
-dog = new dog(40,30);
-dog.addImage("doglmg.png")  
+  database=firebase.database();
+	createCanvas(1000, 400);
+  foodObj = new Food(); 
+  foodStock=database.ref('Food');
+   foodStock.on("value",readStock);
+    dog=createSprite(800,200,150,150);
+     dog.addImage(sadDog); dog.scale=0.15;
+      feed=createButton("Feed the dog");
+       feed.position(700,95); 
+       feed.mousePressed(feedDog);
+        addFood=createButton("Add Food");
+         addFood.position(800,95);
+          addFood.mousePressed(addFoods);
 
-foodStock=database.ref('Food');
-foodStock.on("value",readStock);
 }
 
 
 function draw() {  
 background(46,139,87);
-if(keyWentDown(UP_ARROW)){
-  writeStock(foodS);
-  dog.addImage(dogHappy);
-}
+foodObj.display();
+ fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+     lastFed=data.val(); });
+      fill(255,255,254);
+       textSize(15);
+        if(lastFed>=12){
+           text("Last Feed : "+ lastFed%12 + " PM", 350,30); }
+           else if(lastFed==0){
+              text("Last Feed : 12 AM",350,30); }
+              else{ text("Last Feed : "+ lastFed + " AM", 350,30); }
+
   drawSprites();
-  textSize(30);
-  fill();
-  stroke();
+  
 
 }
 function readStock(data){
   foodS=data.val();
+  foodObj.updateFoodStock(foodS);
 }
-function writeStock(x){
+function feedDog(){
+   dog.addImage(happyDog);
+    foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+     database.ref('/').update({ Food:foodObj.getFoodStock(), FeedTime:hour() }) }
+      //function to add food in stock 
+      function addFoods(){
+         foodS++; database.ref('/').update({ Food:foodS })
+         }
 
-  database.ref('/').update({
-    Food:x
-  })
-}
 
 
 
